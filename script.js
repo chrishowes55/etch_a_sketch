7a4fa2ef7b21575ee327d8e0af451c7c997b11c0
Chris Howes <chris.hs.19.03.03@gmail.com>
2017-04-30 21:56:18 +0100

    calling it a night... fix in morning bitte

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   for(var i = 0; i < 16; i++) {
-    $('.container').append('<tr>');
-    for(var j = 0; j < 15; j++) {
-      $('.container').append("<td class='box'></td>");
+    $('table').append('<tr>');
+    for(var j = 0; j < 16; j++) {
+      $('table').append("<td class='box'></td>");
     }
-    $('.container').append("</tr>");
+    $("table").append("</tr>");
   }
   $('td').hover(function() {
     $(this).css("background-color", "black");
