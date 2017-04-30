db266aa32d7df20c2ac2de0ca85fc112697efb5b
Chris Howes <chris.hs.19.03.03@gmail.com>
2017-04-30 21:27:52 +0100

    Changed to table

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
   for(var i = 0; i < 16; i++) {
-    for(var j = 0; j < 16; j++) {
-      $('.container').append("<div class='box'></div>");
+    $('.container').append('<tr>');
+    for(var j = 0; j < 15; j++) {
+      $('.container').append("<td class='box'></td>");
     }
-    $('.container').append("<div class='endBox'></div>")
+    $('.container').append("</tr>");
   }
-  $('.box').hover(function() {
+  $('td').hover(function() {
     $(this).css("background-color", "black");
   });
-  $('.clearButton').click(function() {
-    var prompt = prompt("How many boxes?");
-    $('.box').remove();
-    for(var i = 0; i < prompt; i++) {
-      for(var j = 0; j < prompt; j++) {
-        $('.container').append("<div class='box'></div>");
-      }
-      $('.container').append("<div class='endBox'></div>")
-    }
-  });
+  // $('.clearButton').click(function() {
+  //   var prompt = prompt("How many boxes?");
+  //   $('.box').remove();
+  //   for(var i = 0; i < prompt; i++) {
+  //     for(var j = 0; j < prompt; j++) {
+  //       $('.container').append("<div class='box'></div>");
+  //     }
+  //     $('.container').append("<div class='endBox'></div>")
+  //   }
+  // });
 });
