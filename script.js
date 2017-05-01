48d277d42f06819e5e517ff59b5c3c40c69ddd2e
Chris Howes <chris.hs.19.03.03@gmail.com>
2017-05-01 13:49:35 +0100

    created the resizing on reset... All works!

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ $(document).ready(function() {
     }
     $("table").append(tr);
   }
+  $('.clearButton').click(function() {
+    var prompt = window.prompt("How many rows and columns would you like?", "16");
+    if(prompt != null) {
+      $('tr').remove();
+      for (var y = 0; y < prompt; y++) {
+        let row = $('<tr />');
+        for(var z = 0; z < prompt; z++) {
+          row.append($('<td />'));
+        }
+        $("table").append(row);
+      }
+      $('td').hover(function() {
+        $(this).css("background-color", "black");
+      });
+    }
+  });
   $('td').hover(function() {
     $(this).css("background-color", "black");
-  });
-  $('.clearButton').click(function() {
-    $('td').css("background-color", "white");
   });
 });
